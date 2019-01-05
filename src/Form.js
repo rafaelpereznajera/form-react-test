26de9f3626542d3fdba55f25a8a1cdddf7170690
rafa perez <rafaelpereznajera@gmail.com>
2019-01-05 12:42:16 +0100

    first commit

---
+++ src/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import FormInput from './FormInput';
+class Form extends Component {
+  render() {
+    return (
+      <form>
+        Form
+        <FormInput />
+      </form>
+    );
+  }
+}
+
+export default Form;
